1e4cc514da520604eef4afdc7426ecf03648f73c
고언약[KR Server Tech] <covenant.ko@kakao.com>
2024-10-20 22:55:35 +0900

    Update gemfile, site

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1729430329',resources: [
+    cacheName: 'chirpy-1729432481',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
