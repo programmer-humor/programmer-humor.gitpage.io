7c6bc0ffe8a1bdab830742d94c438da409e59bad
고언약[KR Server Tech] <covenant.ko@kakao.com>
2024-10-21 14:15:04 +0900

    jekyll-gh-pages.yml 추가

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1729487121',resources: [
+    cacheName: 'chirpy-1729487689',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
